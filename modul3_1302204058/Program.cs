d6fe3047e48eeffe8f9db55abb359f4f0ec78a94
devadelt <devaraja@student.telkomuniversity.ac.id>
2022-03-12 10:05:04 +0700

    menambahkan class KodeBuah

--- modul3_1302204058/Program.cs
+++ modul3_1302204058/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        KodeBuah table_Kodebuah = new KodeBuah();
+        Console.WriteLine("========= GET KODE BUAH APEL =========");
+        table_Kodebuah.getKodebuah("Apel");
+        Console.WriteLine("==== GET ALL KODE BUAH ====");
+        table_Kodebuah.getAllkodebuah();
+        /////////////////////////////////////
+    }
+
+}
+
+
